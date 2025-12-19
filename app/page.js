5a0036f361e63fa78f469666c812fa4772627408
Yujeong <yujeong-yuyu@users.noreply.github.com>
2025-12-20 00:53:17 +0900

    fix: MenuContext import path

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
-import { MenuContext } from "./app/context/MenuContext";
+import { MenuContext } from "../app/context/MenuContext";
 
 import "./global.css";
 
